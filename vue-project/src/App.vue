03c1493930eb7624a82989232658b7ed62da7f2d
Sonia Sunil <37371431+ssunil3232@users.noreply.github.com>
2024-03-24 17:53:54 +0000

    modify filename

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from "./components/HomeView.vue"
 </script>
 
 <template>
-  <HelloWorld msg="You did it!" />
+  <HomeView/>
 </template>
 
 <style scoped>
--- vue-project/src/components/HomeView.vue
+++ vue-project/src/components/HomeView.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+
 </script>
 
 <template>
